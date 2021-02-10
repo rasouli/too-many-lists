14b533436b35ef9388f35f576d39cec44b16a481
rasouli <rasouli@fastmail.com>
2021-02-10 17:18:19 +0330

    cannot replace like this

--- src/first.rs
+++ src/first.rs
@@ -11,3 +11,18 @@ struct Node {
     elem: i32,
     next: Link,
 }
+
+impl List {
+    pub fn new() -> Self {
+        List{ head: Link::Empty}
+    }
+
+    pub fn push(&mut self, value: i32) {
+         let new_node = Node {
+             elem: value,
+             next: self.head,
+         };
+
+        self.head = Link::More(new_node);
+    }
+}
