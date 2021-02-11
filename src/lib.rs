6e373a98be19c6737c3cebf8f368352a96224fef
rasouli <rasouli@fastmail.com>
2021-02-11 14:36:55 +0330

    remove lib tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,6 @@
+use std::fmt::{Debug, Formatter};
+use std::fmt;
+
 // first.rs
 pub mod first;
 pub mod second;
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
