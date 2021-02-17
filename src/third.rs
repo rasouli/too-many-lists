00a44731ab68df2b4d964976894089d90a4782d9
rasouli <rasouli@fastmail.com>
2021-02-17 10:22:08 +0330

    impl head and tail

--- src/third.rs
+++ src/third.rs
@@ -17,4 +17,53 @@ impl <T> List<T> {
             head: None,
         }
     }
+
+    pub fn append(&self, elem: T) -> List<T> {
+        List {
+            head: Some(Rc::new(Node {
+                elem,
+                next: self.head.clone(),
+            }))
+        }
+    }
+
+    pub fn tail(&self) -> List<T> {
+
+        List {
+            // flat map !
+            head: self.head.as_ref().and_then(|node| {
+                node.next.clone()
+            })
+        }
+    }
+
+    pub fn head(&self) -> Option<&T> {
+        self.head.as_ref().map(|node| &node.elem)
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::List;
+
+    #[test]
+    fn basics(){
+        let list = List::new();
+        assert_eq!(list.head(), None);
+
+        let list = list.append(1).append(2).append(3);
+        assert_eq!(list.head(), Some(&3));
+
+        let list = list.tail();
+        assert_eq!(list.head(), Some(&2));
+
+        let list = list.tail();
+        assert_eq!(list.head(), Some(&1));
+
+        let list = list.tail();
+        assert_eq!(list.head(), None);
+
+        let list = list.tail();
+        assert_eq!(list.head(), None);
+    }
 }
